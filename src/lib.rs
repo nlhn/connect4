6ba88ea3b191b3fca33d86f4ef154ea597368e9b
nlhn <nlnguyen@ualberta.ca>
2024-04-12 23:32:19 -0600

    cli added

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,3 @@ mod cli;
 mod otto;
 mod ottobot;
 
-use crate::board::*;
-use wasm_bindgen::prelude::*;
-use crate::otto::TootOttoBoard;
-use std::sync::{Mutex,Arc};
-use lazy_static::lazy_static;
-use wasm_bindgen::JsValue;
-use serde::Serialize;
-
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+mod connect4;
+mod board;
+mod cli;
+mod otto;
+mod ottobot;
+use cli::run_cli;
+
+fn main() {
+    run_cli();
+}
