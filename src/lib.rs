5e99c500b66c4b0bf01fea2e8b5375057acfd3f4
kevin-was-here <code --wait>
2024-04-12 23:32:42 -0600

    remove use

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,3 @@ mod board;
 mod cli;
 mod otto;
 mod ottobot;
-
-use crate::board::*;
-use wasm_bindgen::prelude::*;
-use crate::otto::TootOttoBoard;
-use std::sync::{Mutex,Arc};
-use lazy_static::lazy_static;
-use wasm_bindgen::JsValue;
-use serde::Serialize;
-
